e34c9a364774f53048ddcf8c9665ddcd2f69fc43
LARISSA APARECIDA MACHADO DOS SANTOS <larissaaparecida@univille.br>
2022-05-10 20:41:52 -0300

    Odem de serviço

---
+++ src/main/java/br/univlle/sistemabillyepantcho/entity/ItensOrdemDeServico.java
@@ -0,0 +1,44 @@
+package br.univlle.sistemabillyepantcho.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class ItensOrdemDeServico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private int codProduto;
+    private int qtdFaturado;
+    private float valorTotalItem;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public int getCodProduto() {
+        return codProduto;
+    }
+    public void setCodProduto(int codProduto) {
+        this.codProduto = codProduto;
+    }
+    public int getQtdFaturado() {
+        return qtdFaturado;
+    }
+    public void setQtdFaturado(int qtdFaturado) {
+        this.qtdFaturado = qtdFaturado;
+    }
+    public float getValorTotalItem() {
+        return valorTotalItem;
+    }
+    public void setValorTotalItem(float valorTotalItem) {
+        this.valorTotalItem = valorTotalItem;
+    }
+    
+    
+}
+

a0bfa0cf45e640e43fee9caaab3c999c209bc140
matheus <matheustavares20001@gmail.com>
2022-05-10 20:31:51 -0300

    produto

--- src/main/java/br/univlle/sistemabillyepantcho/entity/OrdemDeServico.java
+++ src/main/java/br/univlle/sistemabillyepantcho/entity/OrdemDeServico.java
@@ -3,11 +3,13 @@ package br.univlle.sistemabillyepantcho.entity;
 import java.util.Date;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class OrdemDeServico {
 
     @Id

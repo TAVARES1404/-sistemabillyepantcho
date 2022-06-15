9295c12aeefd725eb8d57e52e149265d3c1f6572
Matheus Tavares <matheus.tavares@unville.br>
2022-06-14 22:33:22 -0300

    att

--- src/main/java/br/univille/sistemabillyepantcho/controller/ClienteController.java
+++ src/main/java/br/univille/sistemabillyepantcho/controller/ClienteController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.univille.sistemabillyepantcho.dto.ClienteDTO;
-import br.univille.sistemabillyepantcho.entity.Cliente;
 import br.univille.sistemabillyepantcho.service.ClienteService;
 
 @Controller
--- src/main/java/br/univille/sistemabillyepantcho/service/ClienteService.java
+++ src/main/java/br/univille/sistemabillyepantcho/service/ClienteService.java
@@ -3,7 +3,6 @@ package br.univille.sistemabillyepantcho.service;
 import java.util.List;
 
 import br.univille.sistemabillyepantcho.dto.ClienteDTO;
-import br.univille.sistemabillyepantcho.entity.Cliente;
 
 public interface ClienteService {
 
